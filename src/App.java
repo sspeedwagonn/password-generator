10958f529a9b93cfcd8cce87eeed4c8335f2ad22
sspeedwagonn <burnerforcarter@gmail.com>
2023-11-03 14:56:35 -0500

    Adding initial files

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        PasswordGeneratorGUI passwordGeneratorGUI = new PasswordGeneratorGUI();
+
+        passwordGeneratorGUI.setVisible(true);
+    }
+}
